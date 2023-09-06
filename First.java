12c2a64c8746f10f5ad328c3ab662499f2bae95a
Syed Hashir Ali <113060592+Syed-hashir@users.noreply.github.com>
2023-09-06 21:58:13 +0500

    Create First.java
    My First Code in java

---
+++ First.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class First {
+    public static void main(String[]arg){
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Hello World");
+
+        int x = obj.nextInt();
+
+        System.out.println("Out Put is : "+x);
+      
+      
+    
+}
+}
